009bc40c63f292ffd3ad8c5b29bdc10a7fde8228
Balajiravi6300 <30747538+Balajiravi6300@users.noreply.github.com>
2017-08-08 19:16:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,14 +9,14 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/artical-one',function(req,res){
- res.send('Artical one will be served here ');   
+app.get('/article-one',function(req,res){
+ res.send('Article one will be served here ');   
 });
-app.get('/artical-two',function(req,res){
- res.send('Artical two will be served here ');   
+app.get('/article-two',function(req,res){
+ res.send('Article two will be served here ');   
 });
-app.get('/artical-three',function(req,res){
- res.send('Artical three will be served here ');   
+app.get('/article-three',function(req,res){
+ res.send('Article three will be served here ');   
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
