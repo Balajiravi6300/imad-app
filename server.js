9773fd83e26ec7412d162a24eb9b4fda73871a14
Balajiravi6300 <30747538+Balajiravi6300@users.noreply.github.com>
2017-08-15 14:27:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,101 +6,116 @@ var app = express();
 app.use(morgan('combined'));
 
 var articles = {
- 'article-one' : {
-    title: 'Article One Balaji Ravi',
-    heading: `Article One`,
-    date:'Aug 8,2017',
-    content:`<p>
-            This is the content for my first article,This is the content for my first article,This is the content for my first article
-        </p>
-        <p>
-            This is the content for my first article,This is the content for my first article,This is the content for my first article
+    'article-one':  {
+        title: 'Article One | Bi',
+        heading: 'Article One',
+        date: 'Aug 10, 2017',
+        paracontent: `
+            <p>
+                Para 1
+                These are the lines in article one These are the lines in article one These are the lines in article one
+                These are the lines in article one These are the lines in article one These are the lines in article one
             </p>
             <p>
-                This is the content for my first article,This is the content for my first article,This is the content for my first article
-            </p>`
-},
- 'article-two' : { title: 'Article Two Balaji Ravi',
-    heading: `Article Two`,
-    date:'Aug 8,2017',
-    content:`<p>
-            This is the content for my first article,This is the content for my first article,This is the content for my first article
-        </p>
-        <p>
-            This is the content for my firstarticle,This is the content for my first article,This is the content for my first article
+                Para 2
+                These are the lines in article one These are the lines in article one These are the lines in article one
+                These are the lines in article one These are the lines in article one These are the lines in article one
             </p>
             <p>
-                This is the content for my first article,This is the content for my first article,This is the content for my first article
+                Para 3
+                These are the lines in article one These are the lines in article one These are the lines in article one
+                These are the lines in article one These are the lines in article one These are the lines in article one
             </p>`
-},
- 'article-three' : { title: 'Article Three Balaji Ravi',
-    heading: 'Article Three',
-    date:'Aug 8,2017',
-    content:`<p>
-            This is the content for my first article,This is the content for my first article,This is the content for my first article
-        </p>
-        <p>
-            This is the content for my Third article,This is the content for my Thrid article,This is the content for my Third article
-            </p>
+    },
+    'article-two':  {
+        title: 'Article Two | Bi',
+        heading: 'Article Two',
+        date: 'Aug 11, 2017',
+        paracontent: `
             <p>
-                This is the content for my first article,This is the content for my first article,This is the content for my first article
+                Para 1
+                These are the lines in article two
             </p>`
- }
+    },
+    'article-three':  {
+        title: 'Article Three | Bi',
+        heading: 'Article Three',
+        date: 'Aug 12, 2017',
+        paracontent: `
+            <p>
+                Para 1
+                These are the lines in article three
+            </p>`
+    }
 };
- function createTemplate (data){
-     var title = data.title;
-     var date = data.date;
-     var heading = data.heading;
-     var content = data.content;
- 
- var htmlTemplate = `
- <html>
-<head>
-    <title>
-          ${title}
-          </title>
-          <meta name="viewport" content="width=device-width,intial-scale=1"/>
-          <link href="/ui/style.css" rel="stylesheet" />
-  
-    </head>
-<body>
-    <div class="cointainer">
-    <div>
-        <o href="/">Home</o>
-    </div>
-    <hr/>
-    <h3>
-        ${heading}
-    </h3>
-    <div>
-        ${date}
-        </div>
-        <div>
-        ${content}
-    </div>
-    </div>
-</body>
 
-
-
-</html>
-
- 
- 
- 
- `;
- return htmlTemplate;
-     }
- 
+function createTemplate(data) {
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var paracontent = data.paracontent;
+    
+    var htmlTemplate = `
+        <html>
+            <head>
+                <title>
+                    ${title}
+                </title>
+                <meta name="viewport" content="width=device-width, initial-scale=1" >
+                <link href="/ui/style.css" rel="stylesheet" />
+            </head>
+            
+            <body>
+                <div class="container">
+                    <div>
+                        <a href="/">Home</a>
+                    </div>
+                    <hr/>
+                    
+                    <h2>
+                        ${heading}
+                    </h2>
+                    <div>
+                        ${date}
+                    </div>
+                    <div>
+                        ${paracontent}
+                    </div>
+                </div>
+            </body>
+            
+        </html>
+    `;
+    return htmlTemplate;
+}
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui','index.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/:articleName',function(req,res){
-var articleName = req.params.articleName;
- res.send(createTemplate(articles[articleName]));   
+var counter = 0;
+app.get('/counter', function (req, res) {
+    counter = counter + 1;
+    res.send(counter.toString());
 });
+
+app.get('/:articleName', function (req, res){
+   var articleName = req.params.articleName;
+   res.send(createTemplate(articles[articleName]));
+});
+
+// app.get('/article-one', function (req, res){
+//  res.send(createTemplate(articleOne));
+// });
+
+// app.get('/article-two', function (req, res){
+//    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));    
+// });
+
+// app.get('/article-three', function (req, res){
+//    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));    
+//});
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
