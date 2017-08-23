718765c7d11541c61236e7591fbded4e7e15be59
Balajiravi6300 <30747538+Balajiravi6300@users.noreply.github.com>
2017-08-23 21:47:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,12 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
 var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight (){
+ moveLeft = marginLeft  =  10;
+img.style.marginLeft = marginLeft ='px';
+}
 img.onclick = function(){
-    img.style.margin = "100px";
+    var interval = setInterval(moveRight,100);
+    
 };
